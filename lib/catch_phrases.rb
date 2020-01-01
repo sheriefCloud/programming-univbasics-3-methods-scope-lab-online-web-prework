e474fd783a73b7782df578609879b73d47a34157
Sherief Farid <sherief@farid.cloud>
2020-01-01 22:07:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
-def mario
+status = def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
   return status
 end
 
-def toadstool (mario)
+def toadstool (status)
   puts status
 end
